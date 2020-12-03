de5aaa744817b089d82f7305bd12f58f5990ba7f
Shagor Ahmed <70128356+AlorShagor@users.noreply.github.com>
2020-12-03 23:58:23 +0600

    Add files via upload

---
+++ IconStyle/js/active.js
@@ -0,0 +1,19 @@
+$( document ).Ready(function() {
+    
+    $('.slider-active').owlCarousel({
+        loop:true,
+       
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+});
